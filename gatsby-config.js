3adbc444ad89164a5260cce083fa4f6c05cc0369
Ali Haider <gamicacloudgit0@gmail.com>
2021-05-21 13:41:48 +0500

    gatsby blog post site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,35 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: "Ali Haider",
+    description: "I teach JavaScript, React, GraphQL and Gatsby",
+    author: "Alilogic",
+  },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sass",
+  "gatsby-transformer-remark",
+  {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      name: "src",
+      path: `${__dirname}/src/`,
+    },
+  },
+  "gatsby-transformer-sharp",
+  "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 750,
+              linkImagesToOriginal: false,
+            },
+          },
+        ],
+      },
+    },
+],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
-
-export default function Home() {
-  return <div>Hello world!</div>
+import Layout from "../components/layout"
+import Metadata from "../components/metadata"
+const Index = () => {
+  return (
+    <Layout>
+      <Metadata title="Home" description="This is my home page" />
+      <h1>Home page</h1>
+      <h2>I'm Ali Haider, a teacher and a Gatsby.js developer</h2>
+    </Layout>
+  )
 }
+
+export default Index
