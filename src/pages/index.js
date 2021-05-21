96488c21ad9f608bee032d0b9cff3eae62a88a9c
Ali Haider <gamicacloudgit0@gmail.com>
2021-05-21 14:44:34 +0500

    gatsby blog post site

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,38 @@ const Index = () => {
       <h1>Home page</h1>
       <h2>I'm Ali Haider, a teacher and a Gatsby.js developer</h2>
       <h2>Here am created two blog post on my site using gatsby and graphql</h2>
+      <div>
+      Lorem Ipsum into your documents. So we’ve put together a couple of examples for you.
+
+Plain Text
+Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam dictum felis eu pede mollis pretium. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+
+Aenean commodo ligula eget dolor. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Integer tincidunt.
+
+Cras dapibus. Aenean vulputate eleifend tellus. Vivamus elementum semper nisi. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Donec vitae sapien ut libero venenatis faucibus. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
+
+Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.
+
+Maecenas nec odio et ante tincidunt tempus.
+
+Stylized Text
+Lorem ipsum
+
+Dolor sit amet, consectetuer adipiscing elit. Nullam dictum felis eu pede mollis pretium. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+
+Aenean commodo ligula eget dolor. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+
+Donec pede justo
+Fringilla vel
+Aliquet nec
+Vulputate eget
+Arcu.
+In enim justo
+
+Rhoncus ut, imperdiet a, venenatis vitae, justo. Integer tincidunt. Cras dapibus.
+
+Aenean vulputate natoque eleifend tellus. Vivamus elementum semper nisi. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet.
+      </div>
 
     </Layout>
   )
